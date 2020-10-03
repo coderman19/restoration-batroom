4e2e416cccf79dfb6b64069d3d2498a9cbcdb655
coderman19 <greenman966@gmail.com>
2020-10-04 01:26:26 +0300

    add content on main project

--- gulpfile.js
+++ gulpfile.js
@@ -25,7 +25,8 @@ gulp.task('style', function() {
     return gulp.src([
             'node_modules/normalize.css/normalize.css',
             'node_modules/slick-carousel/slick/slick.css',
-            'node_modules/magnific-popup/dist/magnific-popup.css'
+            'node_modules/magnific-popup/dist/magnific-popup.css',
+            'node_modules/@fortawesome/fontawesome-free/css/all.css'
         ])
         .pipe(concat('libs.min.css'))
         .pipe(cssmin())
