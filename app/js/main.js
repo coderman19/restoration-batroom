8d9eb78478b993b9b02f987be316bad3f6809f9f
coderman19 <greenman966@gmail.com>
2020-10-11 04:38:04 +0300

    add slider

--- app/js/main.js
+++ app/js/main.js
@@ -1,12 +1,12 @@
 $(function() {
-
-
-
-
-
-
-
-    new WOW().init();
-
-
+    $("#slider-item").slick({
+        arrows: false,
+        infinite: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        speed: 1000,
+        dots: true
+    });
 });
