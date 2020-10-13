46e252b550a03edae3399b75c516b6df60feef03
coderman19 <greenman966@gmail.com>
2020-10-13 04:55:09 +0300

    add animate effects on projects

--- app/js/main.js
+++ app/js/main.js
@@ -7,6 +7,27 @@ $(function() {
         autoplay: true,
         autoplaySpeed: 2000,
         speed: 1000,
-        dots: true
+        dots: false
     });
+
+    // $(window).scroll(function() {
+    //     $('.mov').each(function() {
+    //         var imagePos = $(this).offset().top;
+    //         var topOfWindow = $(window).scrollTop();
+    //         if (imagePos < topOfWindow + 100) {
+    //             $(this).addClass('rotateIn');
+    //         }
+    //     });
+    // });
+
+
+    wow = new WOW({
+        boxClass: 'wow', // default
+        animateClass: 'animated', // default
+        offset: 120, // default
+        mobile: true, // default
+        live: true // default
+    })
+    wow.init();
+
 });
