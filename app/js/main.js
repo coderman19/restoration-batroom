ff98fe4f920c46bf78d767ee0a2e610d96f79e86
coderman19 <greenman966@gmail.com>
2020-10-27 23:07:36 +0300

    add new content and description

--- app/js/main.js
+++ app/js/main.js
@@ -27,15 +27,22 @@ $(function() {
     //     });
     // });
 
-    $('.popup-modal').magnificPopup({
-        type: 'inline',
-        preloader: false,
-        focus: '#username',
-        modal: true
-    });
-    $(document).on('click', '.popup-modal-dismiss', function(e) {
-        e.preventDefault();
-        $.magnificPopup.close();
+    $('.popup-gallery').magnificPopup({
+        delegate: 'a',
+        type: 'image',
+        tLoading: 'Loading image #%curr%...',
+        mainClass: 'mfp-img-mobile',
+        gallery: {
+            enabled: true,
+            navigateByImgClick: true,
+            preload: [0, 1] // Will preload 0 - before current, and 1 after the current image
+        },
+        image: {
+            tError: '<a href="%url%">The image #%curr%</a> could not be loaded.',
+            titleSrc: function(item) {
+                return item.el.attr('title') + '<small>реставрация ванн</small>';
+            }
+        }
     });
 
 
